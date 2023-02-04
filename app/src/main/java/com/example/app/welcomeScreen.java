0af15403c940e5aa4de7c5834947e97c8ccd79b6
ImperialDuck-dev <mjmuyco13@gmail.com>
2023-02-04 23:13:08 +0800

    Created sign in activity page

--- app/src/main/java/com/example/app/welcomeScreen.java
+++ app/src/main/java/com/example/app/welcomeScreen.java
@@ -2,13 +2,25 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.os.*;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class welcomeScreen extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        Button button = findViewById(R.id.btn_signup);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(welcomeScreen.this,signup.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
