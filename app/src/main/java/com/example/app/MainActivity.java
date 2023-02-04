bb4e9370d9eb0246ff4951a5eb7624bedbf7052d
ImperialDuck-dev <mjmuyco13@gmail.com>
2023-02-04 16:31:23 +0800

    Added splash screen and login

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.os.*;
+
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+    Handler h = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        h.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, welcomeScreen.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+
+
     }
 }
