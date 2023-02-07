07bb23467f1dd8b848b47b6cd1c34de4266b9238
blancojames88 <blancojames88@gmail.com>
2023-02-07 22:34:46 +0800

    added ADD activity
    Created add activity (xml only) that would accept article name and article text

---
+++ app/src/main/java/com/example/app/addArticle.java
@@ -0,0 +1,14 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addArticle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_article);
+    }
+}
