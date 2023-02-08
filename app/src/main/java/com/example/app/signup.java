b09cb024de3eb3ff9846dc332ac24d7cb27e7a9b
Danica <danicnyxx@gmail.com>
2023-02-08 21:19:42 +0800

    db part 1

--- app/src/main/java/com/example/app/signup.java
+++ app/src/main/java/com/example/app/signup.java
@@ -3,12 +3,25 @@ package com.example.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class signup extends AppCompatActivity {
+
+    EditText username, email, password, confirm;
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        username = (EditText) findViewById(R.id.usernameInput);
+        email = (EditText) findViewById(R.id.emailInput);
+        password = (EditText) findViewById(R.id.passwordInput);
+        confirm = (EditText) findViewById(R.id.confirmpasswordInput);
+
+        signup = (Button) findViewById(R.id.btn_signin);
+
     }
 }
