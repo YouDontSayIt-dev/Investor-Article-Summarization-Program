34e2d366f1e86c86321b447cf2ad4b9293d4f9c1
blancojames88 <blancojames88@gmail.com>
2023-02-10 00:11:46 +0800

    added sentiment analysis

--- app/src/main/java/com/example/app/result.java
+++ app/src/main/java/com/example/app/result.java
@@ -6,9 +6,17 @@ import android.os.Bundle;
 
 public class result extends AppCompatActivity {
 
+    private String articleName, articleText, posPercent, negPercent, feedback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        articleName = getIntent().getStringExtra("article_Name");
+//        System.out.println("Article Name: " + articleName);
+
+        articleText = getIntent().getStringExtra("article_Text");
+//        System.out.println("Article Text: " + articleText);
+
     }
 }
