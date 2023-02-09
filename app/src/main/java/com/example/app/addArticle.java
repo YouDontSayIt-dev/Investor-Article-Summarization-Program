92cc4950195b2ab4bf14775fa8b6d7ae6c03a0bd
ImperialDuck-dev <mjmuyco13@gmail.com>
2023-02-09 16:07:26 +0800

    Added button functionality to home

--- app/src/main/java/com/example/app/addArticle.java
+++ app/src/main/java/com/example/app/addArticle.java
@@ -2,13 +2,30 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class addArticle extends AppCompatActivity {
-
+    ImageButton btn_return;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_article);
+        btn_return = (ImageButton) findViewById(R.id.btn_return);
+
+        btn_return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(addArticle.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
     }
 }
