e65af1b14a4c48d9ea8d03fd3c80925bc70ec204
ImperialDuck-dev <mjmuyco13@gmail.com>
2023-02-11 00:46:09 +0800

    Added dashboard article history

--- app/src/main/java/com/example/app/Home.java
+++ app/src/main/java/com/example/app/Home.java
@@ -1,22 +1,37 @@
 package com.example.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
 import android.content.Intent;
+import android.view.MotionEvent;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.ValueEventListener;
+import android.view.LayoutInflater;
+
+import java.util.ArrayList;
 
 
 public class Home extends AppCompatActivity {
     Button addArt;
+    RecyclerView recyclerView;
+    DatabaseReference spid;
+    rViewAdapter rViewAdapter;
+    ArrayList<Articles> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +43,30 @@ public class Home extends AppCompatActivity {
                 Intent intent = new Intent(Home.this, addArticle.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        recyclerView = findViewById(R.id.articleList);
+        spid = FirebaseDatabase.getInstance().getReference("Articles");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        rViewAdapter = new rViewAdapter(this,list);
+        recyclerView.setAdapter(rViewAdapter);
+        spid.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    Articles articles = dataSnapshot.getValue(Articles.class);
+                    list.add(articles);
+                }
+                rViewAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
 
---
+++ app/src/main/java/com/example/app/rViewAdapter.java
@@ -0,0 +1,57 @@
+package com.example.app;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class rViewAdapter extends RecyclerView.Adapter<rViewAdapter.rViewHolder> {
+
+    Context context;
+    ArrayList<Articles> list;
+
+    public rViewAdapter(Context context, ArrayList<Articles> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public rViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).
+                inflate(R.layout.article_item,parent,false);
+        return new rViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull rViewHolder holder, int position) {
+        Articles article = list.get(position);
+        holder.articleName.setText(article.getArticle_Name());
+        holder.percentage.setText(article.getNegPercent());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+
+    public static class rViewHolder extends RecyclerView.ViewHolder{
+
+        TextView articleName,percentage;
+
+        rViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            articleName = itemView.findViewById(R.id.home_art_name);
+            percentage = itemView.findViewById(R.id.home_percent);
+        }
+    }
+}
